736d487197bc3cb44c630d3200eea7ad665e1f08
uzman <127241240+uz-zi@users.noreply.github.com>
2026-07-30 22:56:37 +0100

    Add: is-anagram - submission-12

---
+++ Data Structures & Algorithms/is-anagram/submission-12.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        dic = {}
+        
+        for ch in s:
+            if ch in dic:
+                dic[ch] +=1
+            else:
+                dic[ch] = 1
+
+        for ch in t:
+            if (ch not in dic or dic[ch] <= 0):
+                return False
+            else:
+                dic[ch] -= 1
+
+        return True
+
+            
+
+                
+
+        
